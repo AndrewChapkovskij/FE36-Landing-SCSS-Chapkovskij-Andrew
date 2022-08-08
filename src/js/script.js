27c87a94df42d8807bf5f59d4e079aa66996c7f9
AndrewChapkovkij <andrew.asd.hg@gmail.com>
2022-08-08 23:20:58 +0300

    create responsive

---
+++ src/js/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+  $('.slider__items').slick({
+    arrows: true,
+    dots: true,
+    // adaptiveHeight: true,
+    // slidesToShow: 1,
+    // slidesToScroll: 1,
+    // speed: 300,
+    // easing: 'lianer',
+    // infinity: true,
+    // initialSlide: 0,
+    // autoplay: true,
+    appendDots: $('#slider'),
+    // autoplaySpeed: 3000,
+  })
+})
